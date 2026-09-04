115c7561b5fd4ce73552d3c9ef2a08eacc2a1a21
Akumarin <cozoral@outlook.com>
2026-09-04 12:58:21 +0200

    Small steps

--- Parser.cs
+++ Parser.cs
@@ -6,6 +6,34 @@ namespace Kabuk
 {
     internal class Parser
     {
+	private List<Token> tokens;
+	private int current = 0;
+
+	/* private Expression expression() {
+	
+	return equality();
+	
+	}	
+
+	private Expression equality() {
+	Expression expr = comparison();
+
+	while (match(TokenType.BANG_EQUAL, EQUAL_EQUAL)) {
+	
+	Token operator = previous();
+	Expression right = comparison();
+	// expr = new 
+
+	// Will continue later 	
+	// 04/09/2026
+	}
+	
+	} */
+
+	public Parser(List<Token> tokens) {
+
+	this.tokens = tokens;
+	}
 
     }
 }
